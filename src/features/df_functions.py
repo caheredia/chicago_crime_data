fba6057188e0eecb911233757f9f35c3604b1dc8
caheredia <cristianheredia@gmail.com>
2018-11-01 23:11:08 -0700

    move notebook in heirarchy of files

---
+++ src/features/df_functions.py
@@ -0,0 +1,63 @@
+import pandas as pd
+
+
+def convert_time(data, column_name='Time Occurred'):
+    '''Formats the time column to length of 4.
+
+    Pads the begining of Time stamp to length of 4. Creates a new column with full stamp as a timestamp index.
+
+    Parameters
+    ----------
+    filename : string (optional)
+        location to save the data
+    url : string (optional)
+        web location of the data
+    force_download : bool (optional)
+        if TRUE, then force data download
+
+    Returns
+    -------
+    data : pandas.DataFrame
+        Chicago crime stamps.
+    '''
+    # convert time to string from int
+    data[column_name] = data[column_name].apply(str)
+
+    # pad time for lengths less than 4
+    data[column_name] = data[column_name].apply(str_pad)
+
+    data['Date'] = data['Date Reported'] + " " + data['Time Occurred']
+    data.set_index('Date', inplace=True)
+
+    try:
+        data.index = pd.to_datetime(data.index,  format='%m/%d/%Y %H%M')
+    except TypeError:
+        data.index = pd.to_datetime(data.index)
+
+    return data
+
+
+def str_pad(time_stamp):
+    '''
+    Pads the begining of string with 0 until length 4 is reached.
+
+    Parameters
+    ----------
+    time_stamp : string
+       string for
+    url : string (optional)
+        web location of the data
+    force_download : bool (optional)
+        if TRUE, then force data download
+
+    Returns
+    -------
+    new_string : string
+        padded string or original string
+    '''
+
+    if len(time_stamp) < 4:
+        new_string = str_pad('0' + time_stamp)
+    else:
+        new_string = time_stamp
+    return new_string
--- test_environment.py
+++ test_environment.py
@@ -3,7 +3,7 @@ import sys
 REQUIRED_PYTHON = "python3"
 
 
-def main():
+def test_version():
     system_major = sys.version_info.major
     if REQUIRED_PYTHON == "python":
         required_major = 2
@@ -19,7 +19,3 @@ def main():
                 required_major, sys.version))
     else:
         print(">>> Development environment passes all tests!")
-
-
-if __name__ == '__main__':
-    main()
